81d2e2e8bd0d3cf5704cc01ec57d368abad0581c
João Victor Esc <88864846+Overplayed1@users.noreply.github.com>
2022-05-07 14:21:54 -0300

    Add files via upload

---
+++ MAPS - JavaScript/script.js
@@ -0,0 +1,18 @@
+function getAdmins(map){
+    let admins = [];
+    for ([key, value] of map){
+        if (value === 'Admin'){
+            admins.push(key);
+        }
+    }
+    return admins;
+}
+
+const usuarios = new Map();
+
+usuarios.set('Luiz','Admin');
+usuarios.set('Stephany','Admin');
+usuarios.set('Joao','Admin');
+usuarios.set('Roger','User');
+
+console.log(getAdmins(usuarios));
